7b0b73ddb87a5908ce0cffad771ba50dc92dc46e
ladogerdefi <ladogerdefi@gmail.com>
2024-08-27 21:10:28 +0700

    new method

--- renamer.py
+++ renamer.py
@@ -1,5 +1,3 @@
-### This script renames files in `/images`
-
 import os
 import hashlib
 import shutil
@@ -14,28 +12,37 @@ def hash_file(filepath, blocksize=65536):
     return hasher.hexdigest()[:10]
 
 def rename_files(directory):
-    for filename in os.listdir(directory):
-        filepath = os.path.join(directory, filename)
-        if os.path.isfile(filepath):
-            name, ext = os.path.splitext(filename)
-            if len(name) == 10 and all(c in '0123456789abcdef' for c in name):
-                continue
-            
-            file_hash = hash_file(filepath)
-            new_filename = f"{file_hash}{ext}"
-            new_filepath = os.path.join(directory, new_filename)
-            
-            if filepath != new_filepath:
-                shutil.move(filepath, new_filepath)
-                print(f'Renamed: {filename} to {new_filename}')
+    # Path to the log file in the same directory as this script
+    log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images.txt')
+    
+    with open(log_file_path, 'w') as log:
+        for filename in os.listdir(directory):
+            filepath = os.path.join(directory, filename)
+            if os.path.isfile(filepath):
+                name, ext = os.path.splitext(filename)
+                
+                # Check if the file already has a hash name
+                if len(name) == 10 and all(c in '0123456789abcdef' for c in name):
+                    log.write(f"{filename}\n")
+                    continue
+                
+                file_hash = hash_file(filepath)
+                new_filename = f"{file_hash}{ext}"
+                new_filepath = os.path.join(directory, new_filename)
+                
+                if filepath != new_filepath:
+                    shutil.move(filepath, new_filepath)
+                    log.write(f"{new_filename}  # Renamed from {filename}\n")
+                    print(f'Renamed: {filename} to {new_filename}')
+                else:
+                    log.write(f"{filename}\n")
 
-# Define the path to the images directory explicitly
-# This can be an absolute path or relative to the current working directory
-image_dir = os.path.join(os.getcwd(), "images")  # This assumes 'images' is in the same directory as the script
+# Directory where images are stored
+image_dir = os.path.join(os.getcwd(), "images")
 
 # Ensure the directory exists
 if not os.path.exists(image_dir):
     os.makedirs(image_dir)
 
-# Rename all files in the specified directory
+# Rename all files in the specified directory and log them
 rename_files(image_dir)
